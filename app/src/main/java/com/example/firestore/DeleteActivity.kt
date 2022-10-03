d0012b3daa5f7bd2ae42b10d2b599abcf9455bfd
Bruno Santos Barbosa <brunosantos.2004@outlook.com>
2022-10-03 09:03:23 -0300

    First commit

---
+++ app/src/main/java/com/example/firestore/DeleteActivity.kt
@@ -0,0 +1,70 @@
+package com.example.firestore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.*
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import android.view.View
+
+class DeleteActivity : AppCompatActivity() {
+    private lateinit var db: FirebaseFirestore
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_delete)
+
+        val db = Firebase.firestore
+
+        val btnDeletar: Button = findViewById(R.id.btnDeletar)
+        val btnConfirmar: Button = findViewById(R.id.btnConfirmar)
+
+        val txtPessoa: TextView = findViewById(R.id.txtPessoa)
+
+        val selectID: EditText = findViewById(R.id.edtSelecionarID)
+        val ids: MutableList<String> = mutableListOf()
+
+        var id: String = ""
+
+
+        db.collection("cadastro").get().addOnSuccessListener {
+                result ->
+            for(document in result) {
+                ids.add(document.id)
+            }
+        }
+
+        btnDeletar.setOnClickListener {
+            try{
+                if(selectID.text.toString().toInt() >= ids.size){
+                    Toast.makeText(this, "ID inválido", Toast.LENGTH_SHORT).show()
+                } else {
+                    id = ids[selectID.text.toString().toInt()]
+                    db.collection("cadastro").document(id).get().addOnSuccessListener {
+                            document ->
+                        if(document != null) {
+                            txtPessoa.text = getString(R.string.dados, id, document["nome"], document["cep"], document["bairro"], document["endereco"])
+                            btnConfirmar.visibility = View.VISIBLE
+                        } else {
+                            txtPessoa.text = "Pessoa não encontrada"
+                            btnConfirmar.visibility = View.INVISIBLE
+                        }
+                    }
+                }
+            } catch (e: NumberFormatException) {
+                Toast.makeText(this, "ID inválido", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+        btnConfirmar.setOnClickListener {
+            db.collection("cadastro").document(id)
+                .delete()
+                .addOnSuccessListener { Toast.makeText(this, "Deletado com sucesso", Toast.LENGTH_SHORT).show() }
+                .addOnFailureListener { e -> Toast.makeText(this, "Erro: $e", Toast.LENGTH_SHORT).show() }
+            selectID.text.clear()
+            txtPessoa.text = ""
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/firestore/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.firestore
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnNavegarCadastrar: Button = findViewById(R.id.btnNavegarCadastrar)
+        val btnNavegarConsultar: Button = findViewById(R.id.btnNavegarConsultar)
+        val btnNavegarEditar: Button = findViewById(R.id.btnNavegarEditar)
+        val btnNavegarDeletar: Button = findViewById(R.id.btnNavegarDeletar)
+
+        btnNavegarCadastrar.setOnClickListener {
+            val intent = Intent(this, CreateActivity::class.java)
+            startActivity(intent)
+        }
+        btnNavegarConsultar.setOnClickListener {
+            val intent = Intent(this, ReadActivity::class.java)
+            startActivity(intent)
+        }
+        btnNavegarEditar.setOnClickListener {
+            val intent = Intent(this, UpdateActivity::class.java)
+            startActivity(intent)
+        }
+        btnNavegarDeletar.setOnClickListener {
+            val intent = Intent(this, DeleteActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+
+}
